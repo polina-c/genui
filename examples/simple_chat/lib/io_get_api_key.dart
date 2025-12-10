dcac1635041fe821b07d1421892ee5b0fc08f4c1
Polina Cherkasova <polina.c@live.com>
2025-12-10 14:14:15 -0800

    -

--- examples/simple_chat/lib/io_get_api_key.dart
+++ examples/simple_chat/lib/io_get_api_key.dart
@@ -7,7 +7,7 @@ import 'dart:io';
 /// API key for Google Generative AI (only needed if using google backend).
 /// Get an API key from https://aistudio.google.com/app/apikey
 /// Specify this when running the app with "-D GEMINI_API_KEY=$GEMINI_API_KEY"
-const String geminiApiKey = String.fromEnvironment('GEMINI_API_KEY');
+const String _geminiApiKey = String.fromEnvironment('GEMINI_API_KEY');
 
 String? debugApiKey;
 
@@ -15,9 +15,9 @@ String getApiKey() {
   if (debugApiKey != null) {
     return debugApiKey!;
   }
-  String apiKey = geminiApiKey.isEmpty
+  String apiKey = _geminiApiKey.isEmpty
       ? Platform.environment['GEMINI_API_KEY'] ?? ''
-      : geminiApiKey;
+      : _geminiApiKey;
   if (apiKey.isEmpty) {
     throw Exception(
       '''Gemini API key is required when using google backend. Run the app with a GEMINI_API_KEY as a Dart environment variable, for example by running with -D GEMINI_API_KEY=\$GEMINI_API_KEY or set the GEMINI_API_KEY environment variable in your shell environment.''',
