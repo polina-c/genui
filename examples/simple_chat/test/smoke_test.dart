23b487e4ff42bff599348b8e74638b6b03f58428
Polina Cherkasova <polina.c@live.com>
2025-12-10 14:21:37 -0800

    Update smoke_test.dart

--- examples/simple_chat/test/smoke_test.dart
+++ examples/simple_chat/test/smoke_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2025 The Flutter Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_chat/api_key/io_get_api_key.dart';
 import 'package:simple_chat/main.dart';
