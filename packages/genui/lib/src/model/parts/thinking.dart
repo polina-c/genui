a72c305c1fd3224dad83b798ca56bc5f1c35e7b4
Polina Cherkasova <polina.c@live.com>
2026-01-20 18:50:46 -0800

    -

--- packages/genui/lib/src/model/parts/thinking.dart
+++ packages/genui/lib/src/model/parts/thinking.dart
@@ -1,3 +1,7 @@
+// Copyright 2025 The Flutter Authors.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:genai_primitives/genai_primitives.dart';
 
 /// A provider-specific part for "thinking" blocks.
