f10e0bfd307adeea611c1f9edfe91433d2a6283b
Polina Cherkasova <polina.c@live.com>
2026-01-21 20:34:52 -0800

    Update model.dart

--- packages/genai_primitives/lib/src/parts/model.dart
+++ packages/genai_primitives/lib/src/parts/model.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// @docImport '../parts/parts.dart';
+/// @docImport '../parts/part.dart';
 library;
 
 import 'dart:convert';
