512f79628596b20161f25bf578535af474706d46
Polina Cherkasova <polina.c@live.com>
2026-02-13 17:20:15 -0800

    Update choice_picker.dart

--- packages/genui/lib/src/facade/catalog/items/choice_picker.dart
+++ packages/genui/lib/src/facade/catalog/items/choice_picker.dart
@@ -3,18 +3,8 @@ import 'package:json_schema_builder/json_schema_builder.dart';
 import '../../../../genui.dart';
 import '../model.dart';
 
-sealed class ChoicePickerDefinition extends ComponentDefinition {}
-
-enum ChoicePickerOption {
-  multipleSelectionNoLimit,
-  multipleSelectionWithLimit,
-  mutuallyExclusive,
-}
-
 final class ChoicePickerSchema extends ComponentSchema<ChoicePickerDefinition> {
-  ChoicePickerSchema({
-    List<ChoicePickerOption> allowedOptions = ChoicePickerOption.values,
-  }) : super(_schema(allowedOptions));
+  ChoicePickerSchema() : super(_schema);
 
   @override
   ChoicePickerDefinition parse(JsonMap json) {
@@ -22,48 +12,59 @@ final class ChoicePickerSchema extends ComponentSchema<ChoicePickerDefinition> {
   }
 }
 
-Schema _schema(List<ChoicePickerOption> allowedOptions) {
-  return S.object(
-    properties: {
-      'component': S.string(enumValues: ['ChoicePicker']),
-      'label': S.string(description: 'The label for the group of options.'),
-      'options': S.list(
-        description: 'The list of available options to choose from.',
-        items: S.object(
-          properties: {
-            'label': S.string(
-              description: 'The text to display for this option.',
-            ),
-            'value': S.string(
-              description: 'The value associated with this option.',
-            ),
-          },
-          required: ['label', 'value'],
-        ),
-      ),
-      'value': S.combined(
-        oneOf: [
-          S.string(),
-          S.list(items: S.string()),
-          A2uiSchemas.dataBindingSchema(),
-          A2uiSchemas.functionCall(),
-        ],
-        description: 'The list of currently selected values (or single value).',
-      ),
+sealed class ChoicePickerDefinition extends ComponentDefinition {
+  final Map<String, String> options;
 
-      'variant': S.string(
-        description:
-            'A hint for how the choice picker should be displayed and behave.',
-        enumValues: ['multipleSelection', 'mutuallyExclusive'],
-      ),
-      'filterable': S.boolean(
-        description: 'Whether the options can be filtered by the user.',
-      ),
-      'checks': A2uiSchemas.checkable(),
-    },
-    required: ['component', 'options', 'value'],
-  );
+  ChoicePickerDefinition({required this.options});
 }
+
+final class MultipleChoiceDefinition extends ChoicePickerDefinition {
+  final String? selection;
+
+  MultipleChoiceDefinition({required this.selection, required super.options});
+}
+
+final class ExclusiveChoiceDefinition extends ChoicePickerDefinition {
+  final List<String> selection;
+
+  ExclusiveChoiceDefinition({required this.selection, required super.options});
+}
+
+Schema _schema = S.object(
+  properties: {
+    'component': S.string(enumValues: ['ChoicePicker']),
+    'label': S.string(description: 'The label for the group of options.'),
+    'options': S.list(
+      description: 'The list of available options to choose from.',
+      items: S.object(
+        properties: {
+          'label': S.string(
+            description: 'The text to display for this option.',
+          ),
+          'value': S.string(
+            description: 'The value associated with this option.',
+          ),
+        },
+        required: ['label', 'value'],
+      ),
+    ),
+    'value': S.combined(
+      oneOf: [
+        S.string(),
+        S.list(items: S.string()),
+        A2uiSchemas.dataBindingSchema(),
+        A2uiSchemas.functionCall(),
+      ],
+      description: 'The list of currently selected values (or single value).',
+    ),
+    'variant': S.string(
+      description:
+          'A hint for how the choice picker should be displayed and behave.',
+      enumValues: ['multipleSelection', 'mutuallyExclusive'],
+    ),
+  },
+  required: ['component', 'options', 'value'],
+);
 
 extension type _ChoicePickerData.fromMap(JsonMap _json) {
   Object? get label => _json['label'];
